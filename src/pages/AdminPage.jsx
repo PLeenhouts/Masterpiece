3a529bfa02e3cd4f5e4acb1f07b64b6b895a3daf
PLeenhouts <211506@student.fontys.nl>
2025-12-17 18:35:02 +0100

    Refactor AdminPage layout and add ExampleCardsPanel

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link, useParams } from "react-router-dom";
 
 import AdminForm from "../components/admin/AdminForm.jsx";
 import CardsTable from "../components/admin/CardsTable.jsx";
+import ExampleCardsPanel from "../components/admin/ExampleCardsPanel.jsx";
 
 export default function AdminPage({ cards, onAddCard, onUpdateCard, onDeleteCard }) {
   const [selectedCard, setSelectedCard] = useState(null);
@@ -12,19 +13,27 @@ export default function AdminPage({ cards, onAddCard, onUpdateCard, onDeleteCard
       <h1>“Holocron-Archives” - …Unlock the secrets of the Force… One card at a time.</h1>
       <p>Een virtueel overzicht van Star Wars CCG kaarten</p>
       <hr/>
-      <p>
+      
+      <div>
       <h2>Links</h2>
       <p><Link to="/">Uitloggen als Admin</Link></p>
-      </p>
+      </div>
       <hr/>
       
-      <h2>Admin – Kaarten beheren</h2>
-      <AdminForm
-        selectedCard={selectedCard}
-        onAddCard={onAddCard}
-        onUpdateCard={onUpdateCard}
-        onCancel={() => setSelectedCard(null)}
-      />
+      <div className="admin-top">
+        <div className="admin-left">
+          <AdminForm
+            selectedCard={selectedCard}
+            onAddCard={onAddCard}
+            onUpdateCard={onUpdateCard}
+            onCancel={() => setSelectedCard(null)}
+          />
+        </div>
+
+        <aside className="admin-right">
+          <ExampleCardsPanel />
+        </aside>
+      </div>
 
        <hr/>
       <h2>Overzicht bestaande kaarten</h2>
